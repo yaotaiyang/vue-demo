6d99a8f0e8624674886aada050e98f027fc7366e
yaoxy <yaoxy@mingyuanyun.com>
2018-03-20 18:07:48 +0800

    modify by yaoxy

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import Vue from 'vue';
 import App from './App';
 import router from './router/index';
 import store from './store/index';
-import "babel-polyfill";
+
 Vue.config.productionTip = false;
 /* eslint-disable no-new */
 new Vue({
